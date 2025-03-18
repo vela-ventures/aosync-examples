1360f86a7b9b8cb6ff1e4d48983a8b0806761345
Artem Purundzhan <artempa1607@gmail.com>
2025-03-18 19:04:32 +0000

    chore: remove dynamic import

--- examples/next-react-19-standalone/src/providers/Providers.tsx
+++ examples/next-react-19-standalone/src/providers/Providers.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import dynamic from "next/dynamic";
-const AOSyncProvider = dynamic(
-    () =>
-      import("@vela-ventures/aosync-sdk-react").then(
-        (module) => module.AOSyncProvider
-      ),
-    { ssr: false }
-  );
-// import { AOSyncProvider } from "@vela-ventures/aosync-sdk-react";
+import { AOSyncProvider } from "@vela-ventures/aosync-sdk-react";
 import React, { ReactNode } from "react";
 
 export const ContextProviders = ({ children }: { children: ReactNode }) => {
